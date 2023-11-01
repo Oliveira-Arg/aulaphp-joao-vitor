cfce33585b90b473e704ef703e07ad2a32755be2
João Vitor ʕ •ᴥ•ʔ <136339312+Oliveira-Arg@users.noreply.github.com>
2023-11-01 11:10:11 -0400

    Add files via upload

---
+++ aulaphp/Ex01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <title>Dragon Ball</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+        echo "Dragon Ball\u{1F432}";
+        ?>
+    </h1>
+
+    <p>Goku ganha uma nova transformação</p>
+    
+</body>
+</html>
